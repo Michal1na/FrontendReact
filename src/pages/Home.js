73310baae0a8c68c312083e0753272995e9cfb8f
Michal1na <mbarnas2011@gmail.com>
2023-11-07 14:27:51 +0100

    Fetch recipes from backend

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from "react-router-dom";
 import RecipePrev from "../components/home/Viewall";
 import Recipes from '../components/home/db.json';
 
 export default function Home() {
+    const [recipes, setRecipes] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:3001/recipes/get', {mode: 'cors'})
+            .then(res => res.json()).then(recipe => {setRecipes(recipe)})
+    }, []);
+
     return (
         <>
             <div className="addrecipe">
@@ -13,7 +19,7 @@ export default function Home() {
             </div>
 
             <div className='home'>
-                <RecipePrev recipes={Recipes}/>
+                <RecipePrev recipes={recipes}/>
             </div>
 
         </>
